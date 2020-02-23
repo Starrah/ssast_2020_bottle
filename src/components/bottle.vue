4322e874557525a97f8cefab641f05a02e759923
Shiqing <shiqing-thu18@yandex.com>
2020-02-23 13:42:01 +0800

    实现小瓶子基本交互

--- src/components/bottle.vue
+++ src/components/bottle.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-<!--        我的测试用代码，你删掉即可~-->
-        <img src="../static/logo.png" style="width: 100%" />
+    <div class="bottle-container">
+        <div :class="{ 'bottle': true, 'bottle-clicked': bottleClicked }"
+            :style="'background-image: ' + backgroundImageUrl()"
+            @mousedown="bottleImageOnMouseDown"
+            @click="bottleImageOnClick" />
+        {{ word }}
     </div>
 </template>
 
@@ -17,15 +20,79 @@
         @Prop({type: Number, default: 0})lineIndex: number;//0~4
         @Prop({type: Number, default: 0})columnIndex: number;//0~4
 
+        theValue: number = 0;
+        bottleClicked: boolean = false;
+
         //点击后修改value时，请调用此函数。
         setValue(newValue: number){
             this.$emit("input", newValue);
             //注：不能直接修改prop的值，而是通过向外发送事件、属性传回来实现才可以。
             //父对象保证监听此setValue事件从而修改value的值、
         }
+
+        backgroundImageUrl() {
+            return 'url(../static/bottle_a' + this.value + '.jpg)';
+        }
+
+        bottleImageOnMouseDown(e) {
+            this.bottleClicked = false;
+        }
+
+        bottleImageOnClick(e) {
+            this.theValue = (this.theValue + 1) % 6;
+            this.setValue(this.theValue);
+            this.bottleClicked = true;
+        }
     }
 </script>
 
 <style scoped>
+.bottle-container {
+  background: transparent;
+  text-align: center;
+  font-size: 0.3rem;
+}
+.bottle {
+  background-size: cover;
+  position: relative;
+  bottom: 0;
+  left: 50%;
+  width: 150px;
+  height: 150px;
+  transform: translate(-50%, 0) scaleX(1) scaleY(1);
+  transition: transform ease 0.12s;
+}
+.bottle.bottle-clicked {
+  animation: bottle-squeeze 0.17s cubic-bezier(.23,.8,.3,2.22);
+}
+.bottle:active {
+  transform: translate(-50%, 0) scaleX(1) scaleY(1);
+}
+.bottle:active:hover {
+  transform: translate(-50%, 0) scaleX(1.06) scaleY(0.95);
+}
+@keyframes bottle-squeeze {
+    0% { transform: translate(-50%, 0) scaleX(1.06) scaleY(0.95); }
+  100% { transform: translate(-50%, 0) scaleX(1) scaleY(1); }
+}
 
+.bottle:after {
+  content: "";
+  background: url('../static/bottle_pop.png');
+  display: block;
+  position: absolute;
+  top: 47%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 160px;
+  height: 160px;
+  opacity: 0;
+}
+.bottle.bottle-clicked:after {
+  animation: bottle-pop 0.16s;
+}
+@keyframes bottle-pop {
+    0% { opacity: 1; transform: translate(-50%, -50%) scale(1); }
+  100% { opacity: 0; transform: translate(-50%, -50%) scale(1.5); }
+}
 </style>
